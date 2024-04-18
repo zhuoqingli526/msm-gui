bd6e8902244c2e555bc49a25ad7da6eac5b6b2c9
Ben Purinton <ben.purinton@gmail.com>
2024-04-18 08:08:44 -0700

    suppress warnings in rspec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,6 +34,9 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 
 RSpec.configure do |config|
+  # Suppress RSpec warnings about potential false positives
+  RSpec::Expectations.configuration.on_potential_false_positives = :nothing
+  
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
